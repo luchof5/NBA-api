30d7275ba8c98570eb0e734be02eb204d7432676
Lucho <lucho_l.f@hotmail.com>
2024-07-06 14:36:10 -0300

    Add style header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,20 @@
 import { Link } from "react-router-dom"
+import "./Header.css"
 
 const Header = () => {
   return (
-    <header>
+    <header className="header">
+      <img src="logo-NBA.webp" alt="logo-nba" />
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">Inicio</Link>
           </li>
           <li>
-            <Link to="/teams">Teams</Link>
+            <Link to="/teams">Equipos</Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact">Contacto</Link>
           </li>
         </ul>
       </nav>
